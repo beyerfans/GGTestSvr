a20449fce82e85403c872399472d931b2999ad5e
beyerfans <gordantw@163.com>
2016-09-14 15:46:40 +0800

    modify code

--- GGLib/Platform/GGServer.cpp
+++ GGLib/Platform/GGServer.cpp
@@ -1,8 +1,16 @@
+#include <string>
 #include "GGServer.h"
+
+
+GGServer::GGServer()
+{
+	REG_CMD_HANDLER(close, CloseServer);
+
+}
 
 void GGServer::Run()
 {
-
+	ReadInput();
 
 }
 
@@ -19,7 +27,34 @@ bool GGServer::_Accept()
 	return true;
 }
 
-void GGServer::OnConsoleCmd(const std::string& strcmd)
+
+void GGServer::ReadInput()
 {
+	std::string instr;
+
+	std::getline(std::cin, instr);
+
+	if (instr.size() != 0)
+	{
+		_OnConsoleCmd(instr);
+	}
+
+}
+
+void GGServer::CloseServer()
+{
+
+
+
+}
+
+void GGServer::_OnConsoleCmd(const std::string& strcmd)
+{
+	
+}
+
+void GGServer::_Close(int iSec)
+{
+
 
 }
--- GGLib/Platform/GGServer.h
+++ GGLib/Platform/GGServer.h
@@ -1,34 +1,45 @@
 #pragma once
 
 #include <list>
+#include <unordered_map>
 #include "../Utility/GGThread.h"
 #include "../NetWork/GGTCPSocket.h"
 
+#define REG_CMD_HANDLER(cmd,funHandle)\
+	m_CmdMap.insert(std::make_pair<std::string,GGServer::pFunHandleCmd>(#cmd,GGServer::##funHandle()));
 
 
 class GGServer :public GGThread
 {
 
+	typedef void (*GGServer::pFunHandleCmd) ();
+	
+
 	public:
-		GGServer(){}
+		
+		GGServer();
 		virtual ~GGServer(){}
 
 		virtual void Run();
 
 		bool AddListen();
 
+		
 
+	protected:      
 
-	protected:
+		void ReadInput();
+		virtual void _Close(int iSec);
+		virtual bool _Accept();
 
-        //TODO: Add console input handling      
-        void OnConsoleCmd(const std::string& cmdstr);
+		//Command handling functions
+		void CloseServer();
 
-        
+		//TODO: Add console input handling      
+		virtual void _OnConsoleCmd(const std::string& cmdstr);
 
-		bool _Accept();
 
 		std::list<GGTcpSocket> m_ListenSock;
 		std::list<GGTcpSocket> m_AcceptSock;	
-		
+		std::unordered_map < std::string, GGServer::pFunHandleCmd> m_CmdMap;
 };
